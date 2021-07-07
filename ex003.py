fc87a357d5ea923bd777f043220482971564b71a
lucas <lucas794@hotmail.com>
2021-07-06 22:02:49 -0400

    first commit

---
+++ ex003.py
@@ -0,0 +1,4 @@
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+soma = valor1+valor2
+print("A soma entre {0} e {1} é igual a {2}! ".format(valor1, valor2, soma))
