7cee373c12aff284a38066ce7e03f2f81ae956b4
Lucas Gauna <lucasgsamudio@gmail.com>
2021-07-07 23:45:49 -0400

    subindo exercicio 05

---
+++ ex005.py
@@ -0,0 +1,5 @@
+valor = int(input("Digite um número: "))
+antecessor = valor-1
+sucessor = valor+1
+print("Analisando o valor {}, seu antecessor é {} e o sucessor é {}".format(
+    valor, antecessor, sucessor))
