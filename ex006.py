5ed2ca35d7bd96031befb9e2390265a74704cdce
Lucas Gauna <lucasgsamudio@gmail.com>
2021-07-07 23:46:46 -0400

    subindo exercicio 06

---
+++ ex006.py
@@ -0,0 +1,9 @@
+valor = int(input("Digite um número: "))
+
+valorDobro = valor * 2
+valorTriplo = valor * 3
+raizQuadrada = valor ** (1/2)
+
+print("O dobro de {} vale {}".format(valor, valorDobro))
+print("O triplo de {} vale {}".format(valor,valorTriplo))
+print("A raiz quadrada de {} é igual a {:.2f}".format(valor,raizQuadrada))
