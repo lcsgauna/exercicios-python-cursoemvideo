f131a3f6396f4b751316072cc3e1fe8e520ccf60
Lucas Gauna <lucas794@hotmail.com>
2021-07-13 23:43:50 -0400

    Subindo exercicio 33

--- exercicios-mundo-1/ex033.py
+++ exercicios-mundo-1/ex033.py
@@ -1,2 +1,21 @@
 # Faça um programa que leia três números e
 # mostre qual é o maior e qual é o menor.
+numero1 = int(input(("Primeiro Valor: ")))
+numero2 = int(input("Segundo Valor: "))
+numero3 = int(input("Terceiro Valor: "))
+
+maior = numero1
+menor = numero1
+
+if (numero2>numero1) and (numero2>numero3):
+    maior = numero2
+elif(numero2<numero1) and (numero2<numero3):
+    menor = numero2
+
+if(numero3>numero1) and (numero3>numero2):
+    maior = numero3
+elif(numero3<numero1) and (numero3<numero2):
+    menor=numero3
+
+print("O menor valor digitado foi {}".format(menor))
+print("O maior valor digitado foi {}".format(maior))
