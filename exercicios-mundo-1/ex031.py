6579823fc814ef8d791c823aa2c91d48fa080d72
Lucas Gauna <lucas794@hotmail.com>
2021-07-13 23:43:14 -0400

    Subindo exercicio 31

--- exercicios-mundo-1/ex031.py
+++ exercicios-mundo-1/ex031.py
@@ -2,3 +2,11 @@
 # de uma viagem em Km. Calcule o preço da passagem
 # cobrando R$0,50 por km para viagens de até 200km 
 # e R$0,45 para viagens mais longas.
+distanciaViagem = float(input("Qual é a distância da sua viagem? "))
+if distanciaViagem <=200:
+    preco = distanciaViagem * 0.50
+else:
+    preco = distanciaViagem * 0.45
+#preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+print("Você está prestes a começar uma vigem de {}Km".format(distanciaViagem))
+print("E o preço da sua passagem será de R${:.2f}".format(preco))
