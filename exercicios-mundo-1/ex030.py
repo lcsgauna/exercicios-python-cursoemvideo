5edd912ef1b26c26fb674518a3fe1f2bbbb440d6
Lucas Gauna <lucas794@hotmail.com>
2021-07-13 23:43:01 -0400

    Subindo exercicio 30

--- exercicios-mundo-1/ex030.py
+++ exercicios-mundo-1/ex030.py
@@ -1,2 +1,8 @@
 # Crie um programa que leia um número inteiro
 # e mostre na tela se ele é PAR ou IMPAR.
+numeroEscolhido = int(input("Me diga um número: "))
+
+if numeroEscolhido % 2 == 0:
+    print("O número {} é PAR".format(numeroEscolhido))
+else:
+    print("O número {} é IMPAR".format(numeroEscolhido))
