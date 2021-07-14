00a3b0305fe7dbb9d33421f727909e34ea9ec5f2
Lucas Gauna <lucas794@hotmail.com>
2021-07-13 23:42:38 -0400

    Subindo exercicio 29

--- exercicios-mundo-1/ex029.py
+++ exercicios-mundo-1/ex029.py
@@ -1,3 +1,12 @@
 # Escreva um programa que leia a velocidade de um carro.
 # Se ele ultrapassar 80km/h, mostre uma mensagem dizendo que ele 
 # foi multado. A multa vai custar R$7,00 por cada km acima do limite.
+
+velocidadeCarro = float(input("Qual é a velocidade atual do carro? "))
+
+if velocidadeCarro > 80 :
+    multa = (velocidadeCarro - 80)*7
+    print("MULTADO! Você excedeu o limite permitido de 80km/h")
+    print("Você deve pagar uma multa de R${:.2f}".format(multa))
+else:
+    print("Tenha um bom dia! Dirija com segurança")
