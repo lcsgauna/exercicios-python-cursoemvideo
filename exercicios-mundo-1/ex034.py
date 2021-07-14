abd6a0437b341e2b3a41a95c0253f2d9d8f6f2c7
Lucas Gauna <lucas794@hotmail.com>
2021-07-13 23:44:04 -0400

    Subindo exercicio 34

--- exercicios-mundo-1/ex034.py
+++ exercicios-mundo-1/ex034.py
@@ -3,3 +3,9 @@
 # Para salários superiores a R$1.250,00 calcule
 # um aumento de 10%. Para os inferiores ou iguais.
 # o aumento é de 15%.
+salario = float(input("Qual é o salário do funcionário? R$"))
+if(salario>1250):
+    salarioReajustado = salario+(salario*10/100)
+else:
+    salarioReajustado = salario+(salario*15/100)
+print("Quem ganhava R${:.2f} passa a ganhar R${:.2f}".format(salario,salarioReajustado))
