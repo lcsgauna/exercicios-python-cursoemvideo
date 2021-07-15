e99662689b2dd998596ca71f776b2a49d3df3a00
Lucas Gauna <lucas794@hotmail.com>
2021-07-14 23:16:50 -0400

    Adicionando descrição do exercicio 03

--- exercicios-mundo-1/ex003.py
+++ exercicios-mundo-1/ex003.py
@@ -1,3 +1,5 @@
+# Crie um programa que leia dois números e mostre a soma entre eles
+
 valor1 = int(input("Digite o primeiro valor: "))
 valor2 = int(input("Digite o segundo valor: "))
 soma = valor1+valor2
