d5e963a365b1516dc89dd125ce7aebfe30b9d65f
Lucas Gauna <lucas794@hotmail.com>
2021-07-14 23:17:18 -0400

    Adicionando descrição do exercicio 05

--- exercicios-mundo-1/ex005.py
+++ exercicios-mundo-1/ex005.py
@@ -1,3 +1,6 @@
+# Faça um programa que leia um número inteiro e mostre
+# na tela o seu sucessor e seu antecessor
+
 valor = int(input("Digite um número: "))
 antecessor = valor-1
 sucessor = valor+1
