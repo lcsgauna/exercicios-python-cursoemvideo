fd0307d043bfe3f312e9c0b155f267b8fd1a4992
Lucas Gauna <lucas794@hotmail.com>
2021-07-18 22:04:31 -0400

    Subindo exercicio 41

--- exercicios-mundo-2/ex041.py
+++ exercicios-mundo-2/ex041.py
@@ -3,5 +3,21 @@
 # - Até 9 anos: Mirim
 # - Até 14 anos: Infantil
 # - Até 19 anos: Junior
-# - Até 20 anos: Sênior
+# - Até 25 anos: Sênior
 # - Acima: Master
+from datetime import date
+anoDeNascimento = int(input("Ano de Nascimento: "))
+anoAtual = date.today().year
+idade = anoAtual - anoDeNascimento
+print("O atleta tem {} anos".format(idade))
+
+if idade <= 9:
+    print("Classificação: Mirim")
+elif idade <= 14:
+    print("Classificação: Infantil")
+elif idade <= 19:
+    print("Classificação: Junior")
+elif idade <= 25:
+    print("Classificação: Sênior")
+else:
+    print("Classificação: Master")
