fcff8a89b7b9661e0d21eb61e54ca65dcb9550c0
Lucas Gauna <lucas794@hotmail.com>
2021-07-18 22:04:17 -0400

    Subindo exercicio 40

--- exercicios-mundo-2/ex040.py
+++ exercicios-mundo-2/ex040.py
@@ -3,3 +3,15 @@
 # - Média abaixo de 5.0: Reprovado
 # - Média entre 5.0 e 6.9: Recuperação
 # - Média 7.0 ou superior: Aprovado
+primeiraNota = float(input("Primeira Nota: "))
+segundaNota = float(input("Segunda Nota: "))
+media = (primeiraNota+segundaNota)/2
+
+print("Tirando {:.1f} na primeira nota e {:.1f} na segunda nota a média do aluno é {:.1f}".format(primeiraNota,segundaNota,media))
+
+if media > 7:
+    print("O aluno está aprovado")
+elif media < 5:
+    print("O aluno está reprovado")
+else:
+    print("O aluno está de recuperação")
