871daea77c9ea14494d2d7fff1cddc2d180a22cd
Lucas Gauna <lucas794@hotmail.com>
2021-07-18 22:04:43 -0400

    Subindo exercicio 42

--- exercicios-mundo-2/ex042.py
+++ exercicios-mundo-2/ex042.py
@@ -4,3 +4,19 @@
 # - Isósceles: dois lados iguais
 # - Escaleno: todos os lados diferentes
 
+primeiroSegmento = float(input("Primeiro segmento: "))
+segundoSegmento = float(input("Segundo segmento: "))
+terceiroSegmento = float(input("Terceiro segmento: "))
+
+if(primeiroSegmento<segundoSegmento+terceiroSegmento) and (segundoSegmento<primeiroSegmento+terceiroSegmento) and (terceiroSegmento<primeiroSegmento+segundoSegmento):
+    
+    if primeiroSegmento != segundoSegmento and primeiroSegmento != terceiroSegmento and segundoSegmento != terceiroSegmento:
+        triangulo = "Escaleno"
+    elif primeiroSegmento != segundoSegmento or primeiroSegmento !=  terceiroSegmento or segundoSegmento != terceiroSegmento:
+        triangulo = "Isósceles"
+    else:
+        triangulo = "Equilátero"
+
+    print("Os segmentos acima podem formar um triângulo {}".format(triangulo)) 
+else:
+    print("Os segmentos acima não podem formar triângulo")
