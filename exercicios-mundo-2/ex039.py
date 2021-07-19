0e16da6e48a802132a711c1065ef72cefe42bd85
Lucas Gauna <lucas794@hotmail.com>
2021-07-18 22:03:37 -0400

    Subindo exercicio 39

--- exercicios-mundo-2/ex039.py
+++ exercicios-mundo-2/ex039.py
@@ -5,3 +5,22 @@
 # - Se já passou do tempo de alistamento
 # Seu programa também deverá mostrar o tempo que falta 
 # ou que passou do prazo
+
+from datetime import date
+anoDeNascimento = int(input("Ano de nascimento: "))
+anoAtual = date.today().year
+idade = anoAtual - anoDeNascimento
+
+if idade > 18:
+    tempo = idade - 18
+    anoAlistamento = anoAtual-tempo
+    print("Quem nasceu em {} tem {} em {}".format(anoDeNascimento,idade, anoAtual))
+    print("Você já deveria ter se alistado há {} anos".format(tempo))
+    print("Seu alistamento foi em {}".format(anoAlistamento))
+elif idade < 18:
+    tempo = 18 - idade
+    anoAlistamento = anoAtual + tempo
+    print("Quem nasceu em {} tem {} em {}".format(anoDeNascimento,idade, anoAtual))
+    print("Seu alistamento será em {}".format(anoAlistamento))
+else:
+    print("É o momento do seu alistamento militar")
