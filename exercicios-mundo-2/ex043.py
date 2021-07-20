bf3ed1f17a0d78fc28e5aa5568248b097f0ce9eb
Lucas Gauna <lucas794@hotmail.com>
2021-07-19 23:39:06 -0400

    Subindo exercicio 43

--- exercicios-mundo-2/ex043.py
+++ exercicios-mundo-2/ex043.py
@@ -6,3 +6,21 @@
 # - 30 até 40: Obesidade
 # - Acima de 40: Obesidade mórbida
 
+peso = int(input("Qual é seu peso? (Kg) "))
+altura = float(input("Qual é sua altura? "))
+
+imc = peso/pow(altura,2)
+
+print("O IMC dessa pessoa é de {:.2f}".format(imc))
+
+if imc < 18.5:
+    print("Você está abaixo do peso normal")
+elif imc >= 18.5 and imc <= 25:
+    print("Parabéns, você está na faixa de peso ideal")
+elif imc > 25 and imc <= 30:
+    print("Você está em Sobrepeso")
+elif imc > 30 and imc <= 40 :
+    print("Você está em obesidade")
+elif imc > 40:
+    print("Você está em obesidade morbida, cuidado!")
+
