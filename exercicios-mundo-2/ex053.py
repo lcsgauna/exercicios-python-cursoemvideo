437af7b256e48a6cbc217a073984b52985e2a493
Lucas Gauna <lucas794@hotmail.com>
2021-07-22 23:24:49 -0400

    Adicionando exercicios parte 2 do curso com suas descrições

---
+++ exercicios-mundo-2/ex053.py
@@ -0,0 +1,2 @@
+# Crie um programa que leia uma frase qualquer
+# e diga se ela é um palindromo, desconsiderando os espaços.
---
+++ exercicios-mundo-2/ex056.py
@@ -0,0 +1,2 @@
+# Desenvolva um programa que leia o nome, idade e sexo 
+# de 4 pessoas. No final do programa, mostre:
