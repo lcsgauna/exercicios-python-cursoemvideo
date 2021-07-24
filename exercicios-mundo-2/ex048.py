ecbbc0b9f7a99ca6a4b1129023d17ac9037ea4a2
Lucas Gauna <lucas794@hotmail.com>
2021-07-23 21:02:01 -0400

    Subindo exercicio 48

--- exercicios-mundo-2/ex048.py
+++ exercicios-mundo-2/ex048.py
@@ -1,3 +1,6 @@
 # Faça um programa que calcule a soma entre todos os
 # números impares que são múltiplos de três e que se 
 # encontram no intervalo de 1 até 500.
+for numeros in range(1,501):
+    if (numeros % 2 != 0) and (numeros%3 == 0) :
+        print(numeros)
