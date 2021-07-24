5de96c4c9182d4520b4fb7c6d0c8a02bcf9cbbb5
Lucas Gauna <lucas794@hotmail.com>
2021-07-23 21:02:10 -0400

    Subindo exercicio 49

--- exercicios-mundo-2/ex049.py
+++ exercicios-mundo-2/ex049.py
@@ -1,2 +1,10 @@
 # Refaça o desafio 009, mostrando a tabuada de um número
 # que o usuário escolher, só que agora utilizando o laço for
+
+numero = int(input("Digite um número para ver sua tabuada: "))
+print('-'*12)
+
+for multiplos in range(0,11):
+    print("{} x {:2} = {}".format(numero,multiplos,numero*multiplos))
+
+print('-'*12)
