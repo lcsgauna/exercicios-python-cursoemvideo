d0cf4cfbd926546a2884fc86219e8856d535be14
Lucas Gauna <lucas794@hotmail.com>
2021-07-23 21:00:44 -0400

    Subindo exercicio 46

--- exercicios-mundo-2/ex046.py
+++ exercicios-mundo-2/ex046.py
@@ -1,3 +1,11 @@
 # Faça um programa que mostre na tela uma contagem
 # regressiva para o estouro de fogos de artíficio,indo 
 # de 10 até 0, com uma pausa de 1 segundo entre eles.
+import emoji
+import time
+
+for contagem_regressiva in range(11):
+    print(10-contagem_regressiva)
+    time.sleep(1)
+
+print(emoji.emojize(':tada::confetti_ball::tada: ', use_aliases=True))
