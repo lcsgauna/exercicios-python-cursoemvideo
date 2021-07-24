11d5cb480d19d6f87fe98fbb1fe52a01830b0ea2
Lucas Gauna <lucas794@hotmail.com>
2021-07-23 21:02:37 -0400

    Subindo exercicio 51

--- exercicios-mundo-2/ex051.py
+++ exercicios-mundo-2/ex051.py
@@ -1,3 +1,10 @@
 # Desenvolva um programa que leia o primeiro termo
 # e a razão de uma PA. No final, mostre os 10 primeiros
 # termos dessa progressão.
+
+primeiro_termo = int(input("Informe o primeiro termo: "))
+razao = int(input("Informe a razão da progressão aritmética: "))
+ultimo_termo = primeiro_termo+10
+
+for progressao_aritmetica in range(primeiro_termo,ultimo_termo,razao):
+    print(progressao_aritmetica)
