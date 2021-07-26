c2fa400011d5deb7562da0b93513b452d9f0f203
Lucas Gauna <lucas794@hotmail.com>
2021-07-25 22:28:47 -0400

    Subindo exercicio 52

--- exercicios-mundo-2/ex052.py
+++ exercicios-mundo-2/ex052.py
@@ -1,2 +1,19 @@
 # Faça um programa que leia um número inteiro
 # e diga se ele é ou não um número primo.
+
+numero = int(input("Digite um número: "))
+total = 0
+for buscar_numeros_primos in range(1,numero+1):
+    if numero % buscar_numeros_primos == 0:
+        print('\033[33m',end='')
+        total += 1
+    else:
+        print('\033[31m',end='')
+
+    print('{} '.format(buscar_numeros_primos),end='')
+
+print("\n\033[mO número {} foi divisivel {} vezes".format(numero,total))
+if total == 2:
+    print("E por isso ele é primo")
+else:
+    print("E por isso ele não é primo")
