c956948008dbf0374a96a9510ff44a7151afc2d5
Lucas Gauna <lucas794@hotmail.com>
2021-07-26 23:44:05 -0400

    Subindo exercicio 53

--- exercicios-mundo-2/ex053.py
+++ exercicios-mundo-2/ex053.py
@@ -1,2 +1,14 @@
 # Crie um programa que leia uma frase qualquer
 # e diga se ela é um palindromo, desconsiderando os espaços.
+frase = str(input("Digite uma frase: ")).strip().upper()
+palavras = frase.split()
+junto = ''.join(palavras)
+inverso = ''
+inverso = junto[::-1]
+#for letra in range(len(junto)-1,-1,-1):
+ #   inverso += junto[letra]
+print("O inverso de {} é {}".format(junto, inverso))
+if inverso == junto :
+    print('Temos um palindromo')
+else:
+    print("A frase digitada não é um palindromo")
