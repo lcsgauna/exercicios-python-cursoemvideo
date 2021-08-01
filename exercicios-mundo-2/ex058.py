708d4fd0fce96c2e145d683971d867b2eaa9c6a0
Lucas Gauna <lucas794@hotmail.com>
2021-08-01 00:51:59 -0400

    Adicionando exercicios e enunciados do mundo 02 parte 3

---
+++ exercicios-mundo-2/ex058.py
@@ -0,0 +1,4 @@
+# Melhore o jogo do Desafio 028 onde o computador
+# vai "pensar" em umnúmero entre 0 e 10. Só que agora
+# o jogador vai tentar adivinhar até acertar, mostrando
+# no final quantos palpites forma necessários para vencer
---
+++ exercicios-mundo-2/ex059.py
@@ -0,0 +1,9 @@
+# Crie um programa que leia dois valores e mostre
+# um menu na tela:
+# [1] Somar
+# [2] Multiplicar
+# [3] Maior
+# [4] Novos Números
+# [5] Sair do programa
+# Sua operação deverá realiza a operação solicitada
+# em cada caso.
