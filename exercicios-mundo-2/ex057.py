6e12a73a05f97df943d4a23c0687f26aad35f512
Lucas Gauna <lucas794@hotmail.com>
2021-08-01 01:51:09 -0400

    Subindo exercicio 57

--- exercicios-mundo-2/ex057.py
+++ exercicios-mundo-2/ex057.py
@@ -2,3 +2,10 @@
 # mas só aceite os valores 'M' ou 'F'.
 # Caso esteja errado, peça a digitação novamente
 # até ter um valor correto.
+sexo_invalido = True
+while sexo_valido:
+    sexo = str(input("Informe o sexo: "))
+    if(sexo == 'M' or sexo == 'F'):
+        sexo_valido = False
+    
+print('Programa Finalizado')
