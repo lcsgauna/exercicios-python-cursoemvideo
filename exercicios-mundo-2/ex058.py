2225ba69e28ea1dcfbe360f57f13e135b8bc9c1a
Lucas Gauna <lucas794@hotmail.com>
2021-08-01 01:51:19 -0400

    Subindo exercicio 58

--- exercicios-mundo-2/ex058.py
+++ exercicios-mundo-2/ex058.py
@@ -1,4 +1,14 @@
 # Melhore o jogo do Desafio 028 onde o computador
-# vai "pensar" em umnúmero entre 0 e 10. Só que agora
+# vai "pensar" em um número entre 0 e 10. Só que agora
 # o jogador vai tentar adivinhar até acertar, mostrando
 # no final quantos palpites forma necessários para vencer
+from random import randint
+palpites = 0
+computador = randint(0,11)
+tentativa_adivinhacao = 0
+while tentativa_adivinhacao != computador:    
+    palpites += 1
+    tentativa_adivinhacao = int(input("Informe seu {}º palpite: ".format(palpites))) 
+    
+print("Você acertou, o número era {}".format(computador))
+print("Foram necessários {} palpites para vencer".format(palpites))
