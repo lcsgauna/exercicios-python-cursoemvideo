339505bb44610efbe1fd705220e67a110d008512
Lucas Gauna <lucas794@hotmail.com>
2021-08-06 22:48:12 -0400

    Subindo exercicio 64

--- exercicios-mundo-2/ex060.py
+++ exercicios-mundo-2/ex060.py
@@ -1,3 +1,11 @@
 # Faça um programa que leia um número
 # qualquer e mostre o seu fatorial.
 # Ex: 5! = 5x4x3x2x1=120
+fatorial = 0
+numero = int(input('Digite um numero: '))
+
+while numero > 0:
+    fatorial = numero * numero - 1 
+    print(fatorial)
+    numero = numero - 1 
+print('O fatorial de {} é {} '.format(numero,fatorial))
--- exercicios-mundo-2/ex064.py
+++ exercicios-mundo-2/ex064.py
@@ -3,3 +3,12 @@
 # o valor 999, que é a condição de parada. No final, mostre 
 # quantos números foram digitados e qual foi a soma entre eles
 # (desconsiderando o flag)
+num=0
+cont = 0
+soma=0
+while num != 999:
+    num = int(input("Informe um numero: "))
+    cont+=1
+    soma = soma+num
+
+print("Foram digitados {} numeros e a soma deles é {} ".format(cont,soma-999))
