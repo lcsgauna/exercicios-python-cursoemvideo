7bfd37e12b7a24f04ce4e4e317c5aa7233750408
Lucas Gauna <lucas794@hotmail.com>
2021-08-08 11:23:05 -0400

    Subindo exercicio 60

--- exercicios-mundo-2/ex060.py
+++ exercicios-mundo-2/ex060.py
@@ -1,11 +1,14 @@
 # Faça um programa que leia um número
 # qualquer e mostre o seu fatorial.
 # Ex: 5! = 5x4x3x2x1=120
-fatorial = 0
-numero = int(input('Digite um numero: '))
+fatorial = 1
+numero = int(input('Digite um numero para calcular seu Fatorial: '))
+indice = numero
+print('Calculando {}! = '.format(numero), end='')
+while indice > 0:
+    print('{}'.format(indice),end='')
+    print(' x ' if indice > 1 else ' = ', end='')
+    fatorial *= indice
+    indice -=1
 
-while numero > 0:
-    fatorial = numero * numero - 1 
-    print(fatorial)
-    numero = numero - 1 
-print('O fatorial de {} é {} '.format(numero,fatorial))
+print('{}'.format(fatorial))
