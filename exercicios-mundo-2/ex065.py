3b48852f33c6a3953ec509da97d065a8f82e0080
Lucas Gauna <lucas794@hotmail.com>
2021-08-09 21:13:43 -0400

    Subindo exercicio 65

--- exercicios-mundo-2/ex065.py
+++ exercicios-mundo-2/ex065.py
@@ -2,3 +2,14 @@
 # No final da execução, mostre a média entre todos os valores e qual
 # foi o maior e o menor valores lidos. O programa deve perguntar ao usuário
 # se ele quer ou não continuar a digitar valores.
+soma = 0
+resposta = ''
+contador = 0
+while resposta != 'N':
+    numero = int(input('Digite um numero: '))
+    soma += numero
+    contador += 1 
+    resposta = str(input('Deseja continuar? ')).upper()
+
+media = soma/contador
+print('A soma dos numeros é {} e sua média é {:.2f}'.format(soma,media))
