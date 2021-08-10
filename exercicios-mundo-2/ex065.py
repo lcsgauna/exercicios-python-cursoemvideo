30529875936a910a2707796bdf75fb74fe33b82e
Lucas Gauna <lucas794@hotmail.com>
2021-08-09 21:36:24 -0400

    Subindo exercicio 65

--- exercicios-mundo-2/ex065.py
+++ exercicios-mundo-2/ex065.py
@@ -5,11 +5,19 @@
 soma = 0
 resposta = ''
 contador = 0
+maior = 0
+menor = 10
 while resposta != 'N':
     numero = int(input('Digite um numero: '))
+    if numero > maior:
+        maior = numero
+    elif numero < menor:
+        menor = numero
+    
     soma += numero
     contador += 1 
     resposta = str(input('Deseja continuar? ')).upper()
 
 media = soma/contador
+print('Menor valor {} | Maior valor {}'.format(menor,maior))
 print('A soma dos numeros é {} e sua média é {:.2f}'.format(soma,media))
