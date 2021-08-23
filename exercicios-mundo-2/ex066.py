54ea47cf2531b54191ca4a58b185abcd457cc9e4
Lucas Gauna <lucas794@hotmail.com>
2021-08-22 22:40:23 -0400

    Subindo exercicio 66

--- exercicios-mundo-2/ex066.py
+++ exercicios-mundo-2/ex066.py
@@ -2,3 +2,15 @@
 # O programa só vai parar quando o usuário digitar o valor 999,
 # que é a condição de parada. No final, mostre quantos números forma
 # digitados e qual foi a soma entre eles(desconsiderando o flag)
+valor = 0
+contador = 0
+soma = 0
+while valor != 999:
+    valor = int(input('Digite um valor (999 para parar)'))
+    contador += 1
+    soma += valor
+
+contador-=1
+soma = soma-999
+
+print('A soma dos {} valores foi {}'.format(contador,soma))
